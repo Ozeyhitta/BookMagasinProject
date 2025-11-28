fb66d8c681ce0777614fa53434832d74d96d515d
DinhLoi2104 <dinhloinguyen2010@gmail.com>
2025-11-28 08:42:41 +0700

    fix chatbox bi loi customer service

--- frontend/src/app/components/requestsupport/SupportRequestButton.jsx
+++ frontend/src/app/components/requestsupport/SupportRequestButton.jsx
@@ -6,14 +6,13 @@ import "./requestsupport.css";
 
 export default function SupportRequestButton() {
   const [open, setOpen] = useState(false);
-
   return (
     <>
-      {/* Nút hỗ trợ khách hàng */}
       <div
         className="supportButton"
         onClick={() => setOpen(true)}
-        title="Yêu cầu hỗ trợ"
+        title="Yêu cầu hỗ trợ qua email"
+        aria-label="Yêu cầu hỗ trợ qua email"
       >
         <img
           src="https://img.pikbest.com/png-images/20250602/3d-headset-customer-service-agent-icon_11743443.png!w700wp"
@@ -25,4 +24,4 @@ export default function SupportRequestButton() {
       {open && <SupportRequestWindow onClose={() => setOpen(false)} />}
     </>
   );
-}
+    }
